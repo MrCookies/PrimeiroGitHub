233660bb61ad9aa5c8cecdac4bbd6399fae6f516
dam2 - Romero Portas <a14marcosrp@a11equ29.sanclemente.local>
2015-10-07 12:46:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a14marcosrp/primeirogithub/PrimeiroGitHub.java
@@ -0,0 +1,13 @@
+package com.example.a14marcosrp.primeirogithub;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PrimeiroGitHub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeiro_git_hub);
+    }
+}
